b381d39a8ddebaf55355170d16420cc2837f3009
PunGrumpy <noppakorn.pun@gmail.com>
2022-09-29 21:58:15 +0700

    update logo text and css

--- components/logo.js
+++ components/logo.js
@@ -8,34 +8,28 @@ const LogoBox = styled.span`
   font-size: 18px;
   display: inline-flex;
   align-items: center;
-  height: 30px;
-  line-height: 20px;
   padding: 10px;
 
-  img {
+  > img {
     transition: 200ms ease;
   }
-  `
-
-  // &:hover img {
-  //   transform: rotate(20deg);
-  // }
+`
 
 const Logo = () => {
-  const footPrintImg = `/images/logo${useColorModeValue('', '-dark')}.png`
+  const ghostImage = `/images/logo${useColorModeValue('', '-dark')}.png`
 
   return (
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={30} height={30} alt="logo" />
+          <Image src={ghostImage} width={30} height={30} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
             fontWeight="bold"
             ml={3}
           >
-            &nbsp;&nbsp;Grumpy
+            &nbsp;&nbsp;Noppakorn Kaewsalabnil
           </Text>
         </LogoBox>
       </a>
