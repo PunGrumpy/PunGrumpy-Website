8a1e8476a2a2d9cee71cf8ea5389a9495bcc10d2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-20 20:20:17 +0700

    feat(index): turn to use lists `bio`, `i love`, and `social link`

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,58 @@ import {
 } from 'react-icons/io5'
 import Image from 'next/image'
 
+const bioItems = [
+  {
+    title: 'Born and live in',
+    years: '2002',
+    content: 'Bangkok, Thailand'
+  },
+  {
+    title: 'High School Education at',
+    years: '2015 - 2021',
+    content: 'Sriayudhya School',
+    href: 'http://www.sriayudhya.ac.th/'
+  },
+  {
+    title: 'University Education at',
+    years: '2022 - Present',
+    content: "King Mongkut's Institute of Technology Ladkrabang",
+    href: 'https://www.kmitl.ac.th/'
+  }
+]
+
+const iLoveItems = [
+  { title: 'Coding' },
+  { title: 'Music', href: 'https://www.instagram.com/ansonseabra/' },
+  { title: 'Playing Guitar' },
+  { title: 'Photography', href: 'https://500px.com/p/whatthepunphoto' },
+  { title: 'Custom Keyboard' },
+  { title: 'DevOps' }
+]
+
+const socialLinks = [
+  {
+    href: 'https://github.com/PunGrumpy',
+    label: '@PunGrumpy',
+    icon: <IoLogoGithub />
+  },
+  {
+    href: 'https://discordapp.com/users/353899973252874260',
+    label: '@Grumpy#9760',
+    icon: <IoLogoDiscord />
+  },
+  {
+    href: 'https://www.instagram.com/wtpp_p114/',
+    label: '@wtpp_p114',
+    icon: <IoLogoInstagram />
+  },
+  {
+    href: 'https://www.linkedin.com/in/noppakorn-kaewsalabnil/',
+    label: '@Noppakorn Kaewsalabnil',
+    icon: <IoLogoLinkedin />
+  }
+]
+
 const Home = () => (
   <Layout>
     <Container>
@@ -69,60 +121,41 @@ const Home = () => (
       </Box>
 
       <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
+        <Heading as="h3" variant="section-title" mb={4}>
           Bio
         </Heading>
-        <BioSection>
-          <BioYear>2002</BioYear>
-          Born in Bangkok, Thailand
-        </BioSection>
-        <BioSection>
-          <BioYear>2020</BioYear>
-          Graduated from high school at{' '}
-          <Link
-            as={NextLink}
-            href="http://www.sriayudhya.ac.th/"
-            target="_blank"
-          >
-            Sriayudhya School
-          </Link>
-        </BioSection>
-        <BioSection>
-          <BioYear>2021</BioYear>
-          Start learning programming
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Study at 1st -{' '}
-          <Link as={NextLink} href="https://www.kmitl.ac.th/" target="_blank">
-            King Mongkut&apos;s Institute of Technology Ladkrabang
-          </Link>
-        </BioSection>
+        {bioItems.map((item, index) => (
+          <BioSection key={index}>
+            <BioYear>{item.years}</BioYear>
+            {item.title && <span>{item.title} </span>}
+            {item.href ? (
+              <Link as={NextLink} href={item.href} target="_blank">
+                {item.content}
+              </Link>
+            ) : (
+              <span>{item.content}</span>
+            )}
+          </BioSection>
+        ))}
       </Section>
 
       <Section delay={0.3}>
         <Heading as="h3" variant="section-title">
           I Love
         </Heading>
-        <Paragraph>
-          Coding, Music,{' '}
-          <Link
-            as={NextLink}
-            href="https://www.instagram.com/ansonseabra/"
-            target="_blank"
-          >
-            Singer
-          </Link>
-          , Playing Guitar,{' '}
-          <Link
-            as={NextLink}
-            href="https://500px.com/p/whatthepunphoto"
-            target="_blank"
-          >
-            Photography
-          </Link>
-          , Custom Keyboard, Cyber security
-        </Paragraph>
+        <Box display="flex" flexWrap="wrap">
+          {iLoveItems.map((item, index) => (
+            <Box key={index} textAlign="center">
+              {item.href ? (
+                <Link as={NextLink} href={item.href} target="_blank">
+                  <Paragraph>{item.title}</Paragraph>
+                </Link>
+              ) : (
+                <Paragraph>{item.title}</Paragraph>
+              )}
+            </Box>
+          ))}
+        </Box>
       </Section>
 
       <Section delay={0.3}>
@@ -130,56 +163,15 @@ const Home = () => (
           On the web
         </Heading>
         <List>
-          <ListItem>
-            <Link href="https://github.com/PunGrumpy" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @PunGrumpy
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              href="https://discordapp.com/users/353899973252874260"
-              target="_blank"
-            >
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoDiscord />}
-              >
-                @Grumpy#9760
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://www.instagram.com/wtpp_p114/" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @wtpp_p114
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link
-              href="https://www.linkedin.com/in/noppakorn-kaewsalabnil/"
-              target="_blank"
-            >
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoLinkedin />}
-              >
-                @Noppakorn Kaewsalabnil
-              </Button>
-            </Link>
-          </ListItem>
+          {socialLinks.map(link => (
+            <ListItem key={link.href}>
+              <Link href={link.href} isExternal>
+                <Button variant="ghost" colorScheme="teal" leftIcon={link.icon}>
+                  {link.label}
+                </Button>
+              </Link>
+            </ListItem>
+          ))}
         </List>
       </Section>
     </Container>
