55e6a20a510d9dc7d814287886b324c049ff41f3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-02-03 00:21:51 +0700

    chore(app): add speed-insight for monitor on vercel

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function Website({ Component, pageProps, router }) {
   useEffect(() => {
@@ -28,6 +29,7 @@ function Website({ Component, pageProps, router }) {
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
         <Analytics />
+        <SpeedInsights />
       </Layout>
     </Chakra>
   )
